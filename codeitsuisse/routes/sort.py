1891572b6a844ec72d3a93e4f2c4e77b8a33a9df
kaneishere <bwu009@e.ntu.edu.sg>
2020-09-26 00:44:55 +0800

    AHAHAHAHAHAHAHA

--- codeitsuisse/routes/sort.py
+++ codeitsuisse/routes/sort.py
@@ -11,10 +11,9 @@ logger = logging.getLogger(__name__)
 def sort():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input");
-    result = inputValue.sort() 
+    data.sort() 
     logging.info("My result :{}".format(result))
-    return json.dumps(result);
+    return json.dumps(data);
 
 
 
