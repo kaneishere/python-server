da7c1b691f00c0891132a57b1be27bbbdb1e8425
kaneishere <bwu009@e.ntu.edu.sg>
2020-09-26 10:44:47 +0800

    AHAHAHAHAHAHAHA

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -3,3 +3,4 @@ app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.sort
 import codeitsuisse.routes.salad
+import codeitsuisse.routes.clean_floor
---
+++ codeitsuisse/routes/clean_floor.py
@@ -0,0 +1,65 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+def solve(ar, i = 0):
+    if sum(ar) == 0:
+        return 0
+    
+    mx = 1000000000000000
+    left = mx
+    right = mx
+    if i > 0:
+        add = False
+        if ar[i-1] == 0:
+            ar[i-1] += 1
+            add = True
+        else:
+            ar[i-1] -= 1
+        left = solve(ar, i-1) + 1
+        if add:
+            ar[i-1] -= 1
+        else:
+            ar[i-1] += 1
+    
+    if i < len(ar) - 1:
+        add = False
+        
+        if ar[i+1] == 0:
+            ar[i+1] += 1
+            add = True
+        else:
+            ar[i+1] -= 1
+        right = solve(ar, i+1) + 1
+        if add:
+            ar[i+1] -= 1
+        else:
+            ar[i-1] += 1
+
+    return min(left,right)
+
+
+
+@app.route('/clean_floor', methods=['POST'])
+def clean_floor():
+    data = request.get_json();
+    # logging.info("data sent for evaluation {}".format(data))
+    tests = data["tests"]
+    # logging.info("My result :{}".format(result))
+    n = len(tests)
+    results = { "answers" : {}}
+    for i in range(n):
+        ar = tests[str(i)]["floor"]
+        print(ar)
+        ans = solve(ar)
+        results["answers"][str(i)] = ans
+
+    return json.dumps(results);
+
+
+
