4ef0166adc7d553c3f1bd1269d444db494883978
kaneishere <bwu009@e.ntu.edu.sg>
2020-09-26 13:34:45 +0800

    AHAHAHAHAHAHAHA

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -11,6 +11,12 @@ logger = logging.getLogger(__name__)
 last_guess = {}
 last_sum = 0
 
+w = {'maPomegranate': 59,
+ 'maPineapple': 7,
+ 'maAvocado': 32,
+ 'maRambutan': 43,
+ 'maApple': 31,
+ 'maWatermelon': 83}
 @app.route('/fruitbasket', methods=['POST'])
 def fruitbasket():
     data = request.get_data() 
@@ -19,14 +25,15 @@ def fruitbasket():
     # logging.info("data sent for evaluation {}".format(data))
     # random.seed() 
     # logging.info("My result :{}".format(result))
-
-    
+    ans = 0
+    for key,value in data.items():
+       ans +=  w[key] * value
 
     
     # print(ans)
     # logging.info("current guess: {}".format(guesses))
     # last_sum = ans
-    return str(0) 
+    return str(ans)
 
 
 
