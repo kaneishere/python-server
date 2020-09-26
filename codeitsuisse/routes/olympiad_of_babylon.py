864acef75f4d6f131c8cc23962792591186f4d34
kaneishere <bwu009@e.ntu.edu.sg>
2020-09-26 15:54:49 +0800

    AHAHAHAHAHAHAHA

--- codeitsuisse/routes/olympiad_of_babylon.py
+++ codeitsuisse/routes/olympiad_of_babylon.py
@@ -11,26 +11,41 @@ def solve(data):
     bk,dy=data['books'],data['days']
     bk.sort()
     dy.sort()
+    print(bk)
+    print(dy)
     nb,nd=len(bk),len(dy)
     ans=0
     for i in range(nb):
         tmp=bk[:i+1]
-        for j in range(nd):
-            d=dy[j]
-            dp=[0 for i in range(d+1)]
-            dp[0]=1
-            ind=[[] for i in range(d+1)]
-            for k in range(len(tmp)):
-                for tot in range(d,tmp[k]-1,-1):
-                    if dp[tot]==0 and dp[tot-tmp[k]]:
-                        dp[tot]=1
-                        ind[tot]=[c for c in ind[tot-tmp[k]]]
-                        ind[tot].append(k)
-            for k in range(d,0,-1):
-                if dp[k]:
-                    for c in ind[k][::-1]:
-                        tmp.pop(c)
+        dd=dy[:]
+        mi,cur,pp=1000000000,0,[]
+        for x in range(nd):
+            for j in range(len(dd)):
+                d=dd[j]
+                dp=[0 for i in range(d+1)]
+                dp[0]=1
+                ind=[[] for i in range(d+1)]
+                for k in range(len(tmp)):
+                    for tot in range(d,tmp[k]-1,-1):
+                        if dp[tot]==0 and dp[tot-tmp[k]]:
+                            dp[tot]=1
+                            ind[tot]=[c for c in ind[tot-tmp[k]]]
+                            ind[tot].append(k)
+                for k in range(d,0,-1):
+                    if dp[k]:
+                        if d-k<mi:
+                            mi=d-k
+                            cur=d
+                            pp=[y for y in ind[k][::-1]]
+                        break
+                if mi==0:
                     break
+            for x in pp:
+                tmp.pop(x)
+            dd.remove(cur)
+            mi=1000000000
+            if not tmp:
+                break;
         if not tmp:
             ans=i+1
         else:
@@ -47,15 +62,9 @@ def olympiad_of_babylon():
     return json.dumps(result);
 '''
 
-data={
-    "numberOfBooks": 5,
-    "numberOfDays": 3,
-    "books": [114, 111, 41, 62, 64],
-    "days": [157, 136, 130]
-}
+data={'numberOfDays': 10, 'numberOfBooks': 50, 'books': [70, 57, 40, 35, 27, 29, 61, 53, 54, 71, 28, 49, 72, 64, 44, 56, 47, 66, 29, 32, 42, 51, 53, 43, 69, 48, 68, 73, 28, 55, 77, 63, 60, 35, 33, 51, 67, 79, 31, 29, 37, 31, 65, 50, 39, 75, 62, 35, 80, 26], 'days': [105, 86, 118, 99, 85, 108, 109, 116, 92, 119]}
 print(solve(data))
 '''
-
 
 
 
