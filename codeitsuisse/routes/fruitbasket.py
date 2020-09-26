59aa8c70529045c1a6fcd29597a4a2879943faa1
kaneishere <bwu009@e.ntu.edu.sg>
2020-09-26 13:53:36 +0800

    AHAHAHAHAHAHAHA

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -14,9 +14,9 @@ last_sum = 0
 w = {'maPomegranate': 59,
  'maPineapple': 7,
  'maAvocado': 32,
- 'maRambutan': 43,
+ 'maRambutan': 42,
  'maApple': 31,
- 'maWatermelon': 83}
+ 'maWatermelon': 84} 
 @app.route('/fruitbasket', methods=['POST'])
 def fruitbasket():
     data = request.get_data() 
@@ -26,8 +26,8 @@ def fruitbasket():
     # random.seed() 
     # logging.info("My result :{}".format(result))
     ans = 0
-   #  for key,value in data.items():
-   #     ans +=  w[key] * value
+    for key,value in data.items():
+       ans +=  w[key] * value
 
     
     # print(ans)
