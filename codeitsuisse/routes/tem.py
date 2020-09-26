9d309b062c922c28d13dd4949873234496875dbf
jiaqing23 <jiaqing2303@gmail.com@users.noreply.github.com>
2020-09-27 05:12:25 +0800

    dasd

--- codeitsuisse/routes/tem.py
+++ codeitsuisse/routes/tem.py
@@ -1,15 +1,2 @@
-from flask import Flask
-import flaskext.xmlrpc
-
-app = Flask(__name__)
-
-handler = XMLRPCHandler('api')
-handler.connect(app, '/api')
-
-@handler.register
-def hello(name="world"):
-    if not name:
-        raise Fault("unknown_recipient", "I need someone to greet!")
-    return "Hello, %s!" % name
-
-app.run()
+from langdetect import detect
+print(detect("War doesn't show who's right, just who's left.")=='en')
