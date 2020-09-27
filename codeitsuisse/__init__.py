395d28972900b83e504cad094183b7076461ecce
jiaqing23 <jiaqing2303@gmail.com@users.noreply.github.com>
2020-09-27 10:16:13 +0800

    dasd

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -15,7 +15,7 @@ import codeitsuisse.routes.social_distancing
 import codeitsuisse.routes.optimizedportfolio
 #import codeitsuisse.routes.slsm
 #import codeitsuisse.routes.bored_scribe
-# import codeitsuisse.routes.bored_scribe_copy
+import codeitsuisse.routes.bored_scribe_copy
 import codeitsuisse.routes.snakeladder
 
 import codeitsuisse.routes.yinyang
