d28efa1e6d67fb9dfe8ec5d15fe972206ed54e8d
jiaqing23 <jiaqing2303@gmail.com@users.noreply.github.com>
2020-09-27 11:45:56 +0800

    dasd

--- codeitsuisse/routes/bucket_fill.py
+++ codeitsuisse/routes/bucket_fill.py
@@ -13,10 +13,20 @@ POLYLINE = "{http://www.w3.org/2000/svg}polyline"
 
 
 def solvewater(circles, buckets, pipes):
-    IMG_SIZE = 2048
+
+    IMG_SIZE = 0
+    for b in buckets:
+        for bb in b:
+            IMG_SIZE = max(IMG_SIZE, bb[0], bb[1])
+
+    for b in pipes:
+        for bb in b:
+            IMG_SIZE = max(IMG_SIZE, bb[0], bb[1])
+
     img = [[-1 for i in range(IMG_SIZE+5)] for j in range(IMG_SIZE+5)]
     G = [list() for j in range(len(buckets)+5)]
     area = [0 for j in range(len(buckets)+5)]
+
 
     for b in range(len(buckets)):
 
@@ -26,13 +36,14 @@ def solvewater(circles, buckets, pipes):
         if(left > right):
             buc[0], buc[2] = buc[2], buc[0]
             buc[1], buc[3] = buc[3], buc[1]
+            left, right = right, left
 
         for i in range(top, bottom+1):
+            
             img[left][i] = b
             img[right][i] = b
         for i in range(left, right+1):
             img[i][bottom] = b
-
         area[b] = (right-left-1)*(bottom-top)
         
 
@@ -45,7 +56,7 @@ def solvewater(circles, buckets, pipes):
             if(img[up[0]][i] != -1):
                 src = img[up[0]][i]
                 break
-        for i in range(down[1], 2049):
+        for i in range(down[1], IMG_SIZE):
             if(img[down[0]][i] != -1):
                 tgt = img[down[0]][i]
                 break
@@ -53,7 +64,7 @@ def solvewater(circles, buckets, pipes):
             G[src].append(tgt)
 
     startbuc = -1
-    for i in range(circles[1], 2049):
+    for i in range(circles[1], IMG_SIZE):
         if(img[circles[0]][i] != -1):
             startbuc = img[circles[0]][i]
             break
@@ -63,7 +74,7 @@ def solvewater(circles, buckets, pipes):
         left, right, top, bottom = buc[0][0], buc[2][0], buc[0][1], buc[2][1]
         tgt = -1
 
-        for i in range(bottom+1, 2049):
+        for i in range(bottom+1, IMG_SIZE):
             if(img[left][i] != -1):
                 tgt = img[left][i]
                 break
@@ -73,7 +84,7 @@ def solvewater(circles, buckets, pipes):
 
         tgt = -1
         
-        for i in range(bottom+1, 2049):
+        for i in range(bottom+1, IMG_SIZE):
             if(img[right][i] != -1):
                 tgt = img[right][i]
                 break
@@ -106,8 +117,8 @@ def solvewater(circles, buckets, pipes):
 
     ans =0
 
-    for i in range(2049):
-        for j in range(2049):
+    for i in range(IMG_SIZE):
+        for j in range(IMG_SIZE):
             if img[i][j]!=-1 and visited[img[i][j]]==1:
                     ans += 1
 
